10a0df05f597dfd6fbe378ce76a4f60c08004539
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-10-01 14:21:45 -0300

    First commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <a>Link 1</a>
+        <a>Link 2</a>
+        <a><b>LOGO</b></a>
+        <a>Link 3</a>
+        <a>Link 4</a>
+      </header>
+    )
+  }
+}
